7077a7bab8e4b613cbfbe58ec76912292aebe9cf
Václav Makeš <info@makes.cz>
2013-07-19 00:10:00 +0200

    add dependence for documentation - Apigen

---
+++ dev/documentation.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PATH="/var/www/emails"
+SRC="${PATH}/vendor/apigen/apigen/apigen.php"
+DOC="${PATH}/doc"
+
+/usr/bin/php $SRC -s "$PATH/src" -d "$DOC/project"
+/usr/bin/php $SRC -s "$PATH/vendor/shuber/curl" -d "$DOC/lib-curl"
+/usr/bin/php $SRC -s "$PATH/vendor/dg/dibi" -d "$DOC/lib-dibi"
